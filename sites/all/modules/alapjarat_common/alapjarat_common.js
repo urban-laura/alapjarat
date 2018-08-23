3667fb8956e0a3586932479564005f76a4b96545
Borisz <borisz.voros@cheppers.com>
2018-08-23 13:57:40 +0200

    migrate fix, hambi menu

--- sites/all/modules/alapjarat_common/alapjarat_common.js
+++ sites/all/modules/alapjarat_common/alapjarat_common.js
@@ -2,9 +2,9 @@
   Drupal.behaviors.exampleModule = {
     attach: function (context, settings) {
       $('#main_menu ul.menu').prepend('<img src="/sites/all/modules/alapjarat_common/images/alapjarat_hamburger.png" id="ham-menu" style="padding: 4px;cursor: pointer;">');
-      $('.pane-most-popular-terms').hide();
+      $('#mini-panel-hamburger_menu').hide();
       $('#ham-menu').click(function() {
-      	$('.pane-most-popular-terms').slideToggle();
+      	$('#mini-panel-hamburger_menu').slideToggle();
       });
     }
   };
