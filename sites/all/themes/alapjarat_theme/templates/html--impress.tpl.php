063628d019b27694de251ce9dc9622a3617079df
urbanlaura <urban.laura97@gmail.com>
2018-11-21 12:29:12 +0100

    impress

---
+++ sites/all/themes/alapjarat_theme/templates/html--impress.tpl.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ */
+?>
+<!DOCTYPE html>
+<html<?php print $html_attributes . $rdf_namespaces; ?>>
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  <meta property="og:image" content="https://alapjarat.hu/sites/all/themes/alapjarat_theme/images/impress.jpg">
+  <meta property="og:description" content="Az Alapjárat szerkesztősége">
+  <meta property="fb:app_id" content="658926237814717">
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes; ?>>
+  <div id="fb-root"></div>
+
+<!--Facebook-->
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v3.1&appId=658926237814717&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  
+
+  <div id="skip">
+    <a href="#content"><?php print t('Jump to Navigation'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
