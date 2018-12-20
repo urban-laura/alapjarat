5ecbe1f29fc2b82e136dbdb6b1bc3c375508d7b1
Borisz <borisz.voros@cheppers.com>
2018-12-20 20:33:13 +0100

    article status change js fixed

--- sites/all/modules/alapjarat_common/alapjarat_common_aarticle_form.js
+++ sites/all/modules/alapjarat_common/alapjarat_common_aarticle_form.js
@@ -1,16 +1,18 @@
 (function ($) {
   Drupal.behaviors.aarticle_form = {
     attach: function (context, settings) {
-      $('.node-form-options input').click(function() {
-      	var options = ["status", "sketch", "scheduled"];
-      	var index = $.inArray($(this).attr('name'), options);
-		if (index != -1) {
-		  options.splice(index, 1);
-		}
-		options.forEach(function(element, index, array) {
-		  $('input[name="' + element + '"]').attr("checked", false);
-		});
-      	
+      $('.node-form-options input[type="checkbox"]').click(function() {
+        console.log($(this).attr("name"));
+        if ($(this).attr("name") == 'status' || $(this).attr("name") == 'sketch' || $(this).attr("name") == 'scheduled') {
+          var options = ["status", "sketch", "scheduled"];
+          var index = $.inArray($(this).attr('name'), options);
+          if (index != -1) {
+            options.splice(index, 1);
+          }
+          options.forEach(function(element, index, array) {
+            $('input[name="' + element + '"]').attr("checked", false);
+          });
+        }
       });
     }
   };
