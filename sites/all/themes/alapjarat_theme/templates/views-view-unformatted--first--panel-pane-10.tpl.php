ded0f5ea73fc8e15aceec272766f291812b0793d
Borisz <borisz.voros@cheppers.com>
2019-01-19 20:39:58 +0100

    fix article in header region feature fixed

---
+++ sites/all/themes/alapjarat_theme/templates/views-view-unformatted--first--panel-pane-10.tpl.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+
+if (!empty($fixed_article)) {
+  $o1 = $rows[1];
+  $o2 = $rows[2];
+
+  $rows[1] = $rows[3];
+  $rows[2] = $o1;
+  $rows[3] = $o2;
+}
+
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div<?php if ($classes_array[$id]): ?> class="<?php print $classes_array[$id]; ?>"<?php endif; ?>>
+    <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
