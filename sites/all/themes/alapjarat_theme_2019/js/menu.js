cd6111bf053d5d8977b95744505f0149c3cf02b2
urbanlaura <urban.laura97@gmail.com>
2019-03-12 15:32:57 +0100

    responsive 870px

--- sites/all/themes/alapjarat_theme_2019/js/menu.js
+++ sites/all/themes/alapjarat_theme_2019/js/menu.js
@@ -1,16 +1,27 @@
 (function ($) {
   Drupal.behaviors.hamburger = {
     attach: function (context, settings) {
-      $('#main_menu ul.menu').prepend('<a id="ham-menu" style="cursor: pointer;">Hírek</a>');
+      $('#main_menu').prepend('<a id="ham-menu" style="cursor: pointer;">Hírek</a>');
       $('#mini-panel-hamburger_menu_2019').hide();
       $('#ham-menu').click(function() {
-      	$('#mini-panel-hamburger_menu_2019').slideToggle();
+      	$('#mini-panel-hamburger_menu_2019').slideToggle(0);
       	$('#ham-menu').toggleClass("hm-active");
+      });
+      $('#main_menu').prepend('<div id="mobil-menu"></div>');
+      $('#mini-panel-mobil_menu').hide();
+      $('#mobil-menu').click(function() {
+        $('#mini-panel-mobil_menu').slideToggle();
+      });
+      $('#mini-panel-mobil_menu .pane-content').hide();
+      $('#heading .pane-title').click(function() {
+        $('#heading .pane-content').slideToggle();
+      });
+      $('#calculator .pane-title').click(function() {
+        $('#calculator .pane-content').slideToggle();
+      });
+      $('#most-popular .pane-title').click(function() {
+        $('#most-popular .pane-content').slideToggle();
       });
     }
   };
 }(jQuery));
-
-
-
-
