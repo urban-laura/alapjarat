f86f4f19ae29bad27ba1717a08ddcde58eed9118
Borisz <borisz.voros@cheppers.com>
2019-03-13 21:39:13 +0100

    fix article test values

--- sites/all/themes/alapjarat_theme/templates/html.tpl.php
+++ sites/all/themes/alapjarat_theme/templates/html.tpl.php
@@ -12,23 +12,6 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-
-  <!-- Atmedia script -->
-  <script type="text/javascript" src="//gemhu.adocean.pl/files/js/ado.js"></script>
-  <script type="text/javascript">
-  /* (c)AdOcean 2003-2019 */
-          if(typeof ado!=="object"){ado={};ado.config=ado.preview=ado.placement=ado.master=ado.slave=function(){};}
-          ado.config({mode: "old", xml: false, consent: true, characterEncoding: true});
-          ado.preview({enabled: true});
-  </script>
-
-  <script type="text/javascript">
-  /* (c)AdOcean 2003-2019, MASTER: alapjarat.hu.teszt */
-  ado.master({id: 'w0D8GanBjeVLul4hdqqyvqK8n.yv.z.VJ02vJJLUHYL.M7', server: 'gemhu.adocean.pl' });
-  </script>
-  <!-- Atmedia script -->
-
-
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
   <div id="fb-root"></div>
@@ -50,14 +33,6 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-  
-  <!-- Atmedia script -->
-  <div id="adoceangemhuyaetlpkfde"></div>
-  <script type="text/javascript">
-  /* (c)AdOcean 2003-2019, alapjarat.hu.teszt.extra */
-    ado.slave('adoceangemhuyaetlpkfde', {myMaster: 'w0D8GanBjeVLul4hdqqyvqK8n.yv.z.VJ02vJJLUHYL.M7' });
-  </script>
-  <!-- Atmedia script -->
-  
+
 </body>
 </html>
