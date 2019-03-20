69ab571b9f0505f2035f74e3a15bf9e71020f113
Borisz <borisz.voros@cheppers.com>
2019-03-20 08:38:06 +0100

    media gallery theming added

--- sites/all/modules/alapjarat_mc/alapjarat_mc.js
+++ sites/all/modules/alapjarat_mc/alapjarat_mc.js
@@ -2,7 +2,21 @@
   Drupal.behaviors.alapjarat_mc = {
     attach: function (context, settings) {
     
+      $('.alapjarat-mc-gallery-item-video .mc-video').hover(
+ 		function() {
+ 			$(this).parent().next().show();
+ 		}, function() {
+ 			$(this).parent().next().hide();
+ 		}
+      );
 
+      $('.alapjarat-mc-gallery-item-image .mc-image').hover(
+ 		function() {
+ 			$(this).next().show();
+ 		}, function() {
+ 			$(this).next().hide();
+ 		}
+      );
 
 
     }
