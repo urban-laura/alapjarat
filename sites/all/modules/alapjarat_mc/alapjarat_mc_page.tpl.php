a02c6b8461582493a3ec2072444d564342b770d7
Borisz <borisz.voros@cheppers.com>
2019-03-22 08:06:06 +0100

    media collection header menu active class added

--- sites/all/modules/alapjarat_mc/alapjarat_mc_page.tpl.php
+++ sites/all/modules/alapjarat_mc/alapjarat_mc_page.tpl.php
@@ -1,13 +1,13 @@
 <div id="alapjarat-mc-page">
 	<div id="alapjarat-mc-header-menu">
 		<span class="alapjarat-mc-header-menu-item item-images">
-			<a href="/media-collection/image">Images</a>
+			<a href="/media-collection/image" class="###classes_image###">Images</a>
 		</span>
 		<span class="alapjarat-mc-header-menu-item item-all">
-		  <a href="/media-collection/all">All</a>
+		  <a href="/media-collection/all" class="###classes_all###">All</a>
 		</span>
 		<span class="alapjarat-mc-header-menu-item item-video">
-		  <a href="/media-collection/video">Videos</a>
+		  <a href="/media-collection/video" class="###classes_video###">Videos</a>
 		</span>
 	</div>
 	<div id="alapjarat-mc-content">
