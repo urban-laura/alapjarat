6df6575d9432e0de5b785fa78fdd30c1bd7dd4a3
Borisz <borisz.voros@cheppers.com>
2019-03-26 12:04:17 +0100

    media collection gallery images in group, admin config page added

--- sites/all/modules/alapjarat_mc/alapjarat_mc_page.tpl.php
+++ sites/all/modules/alapjarat_mc/alapjarat_mc_page.tpl.php
@@ -1,13 +1,13 @@
 <div id="alapjarat-mc-page">
 	<div id="alapjarat-mc-header-menu">
 		<span class="alapjarat-mc-header-menu-item item-images">
-			<a href="/media-collection/image" class="###classes_image###">Images</a>
+			<a href="/media-collection/image" class="###classes_image###">###header_menu_image###</a>
 		</span>
 		<span class="alapjarat-mc-header-menu-item item-all">
-		  <a href="/media-collection/all" class="###classes_all###">All</a>
+		  <a href="/media-collection/all" class="###classes_all###">###header_menu_all###</a>
 		</span>
 		<span class="alapjarat-mc-header-menu-item item-video">
-		  <a href="/media-collection/video" class="###classes_video###">Videos</a>
+		  <a href="/media-collection/video" class="###classes_video###">###header_menu_video###</a>
 		</span>
 	</div>
 	<div id="alapjarat-mc-content">
