904369c6a11fca315b0368c8e972a41a95180ab1
Borisz <borisz.voros@cheppers.com>
2019-03-26 12:04:17 +0100

    media collection gallery images in group, admin config page added

--- sites/all/modules/alapjarat_mc/alapjarat_mc_image.tpl.php
+++ sites/all/modules/alapjarat_mc/alapjarat_mc_image.tpl.php
@@ -1,6 +1,13 @@
-<div class="alapjarat-mc-gallery-item alapjarat-mc-gallery-item-image">
+<div class="alapjarat-mc-gallery-item alapjarat-mc-gallery-item-image <?php if (isset($item['siblings'])) { print 'gallery'; } ?>">
   <div class="item-image"><?php print $item['thumbnail'];?></div>
-  <a class="mc-image" href="<?php print $item['destination']; ?>" rel="lightbox[roadtrip]">
+  <a class="mc-image" href="<?php print $item['destination']; ?>" rel="<?php print $item['groupName']; ?>">
   </a>
+  <?php
+	if (isset($item['siblings'])) {
+		foreach ($item['siblings'] as $sibling) {
+			print('<a href="' . $sibling .'" rel="' . $item['groupName'] . '" style="display: none;"></a>');
+		}
+	}
+  ?>
   <div class="item-title"><?php print $item['title'];?></div>
 </div>
