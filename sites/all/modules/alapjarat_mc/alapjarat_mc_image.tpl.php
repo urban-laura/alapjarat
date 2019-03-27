eca293abe8d0cdaa809dd5a78aac02f160e84451
Borisz <borisz.voros@cheppers.com>
2019-03-27 09:46:28 +0100

    node nid put back, check plain field_title2 value to strip tags

--- sites/all/modules/alapjarat_mc/alapjarat_mc_image.tpl.php
+++ sites/all/modules/alapjarat_mc/alapjarat_mc_image.tpl.php
@@ -1,4 +1,4 @@
-<div class="alapjarat-mc-gallery-item alapjarat-mc-gallery-item-image <?php if (isset($item['siblings'])) { print 'gallery'; } ?>">
+<div class="alapjarat-mc-gallery-item alapjarat-mc-gallery-item-image node-<?php print $item['content']; ?> <?php if (isset($item['siblings'])) { print 'gallery'; } ?>">
   <div class="item-image"><?php print $item['thumbnail'];?></div>
   <a class="mc-image" href="<?php print $item['destination']; ?>" rel="<?php print $item['groupName']; ?>">
   </a>
